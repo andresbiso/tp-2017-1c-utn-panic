09857606cdfc69c5de6bcde9c438f070dce4c69f
andresbiso <biso.andres@gmail.com>
2017-04-14 10:42:01 -0300

    [Remove] Elimina debug panicommons

--- PANICommons/Debug/panicommons/panilogger.d
+++
@@ -1,4 +0,0 @@
-panicommons/panilogger.d: ../panicommons/panilogger.c \
- ../panicommons/panilogger.h
-
-../panicommons/panilogger.h:
--- PANICommons/Debug/panicommons/panisocket.d
+++
@@ -1,4 +0,0 @@
-panicommons/panisocket.d: ../panicommons/panisocket.c \
- ../panicommons/panisocket.h
-
-../panicommons/panisocket.h:
