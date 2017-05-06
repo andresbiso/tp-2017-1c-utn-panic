79ea29661a229d0627952efd0bd37089a0868596
soad85 <ariboya85@gmail.com>
2017-05-06 13:52:37 -0300

    Agrega chequeo de multiprogramacion

--- Kernel/src/Kernel.h
+++ Kernel/src/Kernel.h
@@ -13,6 +13,7 @@
 #include <commons/collections/queue.h>
 #include <panicommons/serializacion.h>
 #include <panicommons/panicommons.h>
+#include <commons/log.h>
 
 int PuertoConsola;
 int PuertoCpu;
@@ -46,15 +47,41 @@ typedef struct {
 	t_queue* cola;
 } t_semaforo;
 
+typedef struct{
+	int pid;
+	int socket;
+	bool corriendo;
+}t_consola;
+
+typedef struct{
+	t_cpu *cpu;
+	t_consola *programa;
+}t_relacion;
+
 modo_planificacion Modo;
 
+t_log *logNucleo;
+
 int ultimoPID;
-pthread_mutex_t	mutexPID;
+pthread_mutex_t	mutexPID = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutexKernel = PTHREAD_MUTEX_INITIALIZER;
 
 t_config* cargarConfiguracion(char* archivo);
 void cargar_varCompartidas();
 void crear_semaforos();
 t_pcb* armar_nuevo_pcb(char* codigo);
 void inicializar_programa(t_pcb* nuevo_pcb);
+void cargar_programa(int32_t socket, int pid);
+void relacionar_cpu_programa(t_cpu *cpu, t_consola *programa, t_pcb *pcb);
+void liberar_una_relacion(t_pcb *pcb_devuelto);
+void liberar_una_relacion_porsocket_cpu(int socketcpu);
+t_consola* matchear_consola_por_pid(int pid);
+t_relacion* matchear_relacion_por_socketcpu(int socket);
+void elminar_consola_por_socket(int socket);
+void elminar_consola_por_pid(int pid);
+bool esta_libre(void * unaCpu);
+t_log* crearLog();
+t_log* crearLogEstados();
+void programa(void* arg);
 
 #endif /* KERNEL_H_ */
