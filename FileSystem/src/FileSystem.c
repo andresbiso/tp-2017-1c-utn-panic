6de958488d5abc7ffc888efec9ac88ac686ca2f9
Juan Manuel Crespo <juanmacrespo87@gmail.com>
2017-05-21 18:49:08 -0300

    Agrego funciones basicas del FS crear borrar verificar leer #8

--- FileSystem/src/FileSystem.c
+++ FileSystem/src/FileSystem.c
@@ -4,10 +4,12 @@
 #include "FileSystem.h"
 #include <commons/config.h>
 
-void error(char** args){
+void error(char** args)
+{
 	printf("%s",args[0]);
 }
-void mostrarMensaje(char* mensajes,int socket){
+void mostrarMensaje(char* mensajes,int socket)
+{
  	printf("Mensaje recibido: %s \n",mensajes);
 }
 t_config* cargarConfiguracion(char* nombreDelArchivo){
@@ -23,13 +25,60 @@ t_config* cargarConfiguracion(char* nombreDelArchivo){
 	 }
 	if (config_has_property(configFile,"PUNTO_MONTAJE")){
 		punto_montaje= config_get_string_value(configFile,"PUNTO_MONTAJE");
+		rutaBloques= strcat(punto_montaje, "bloques/");
 	}else {
 		perror("La key PUNTO_MONTAJE no existe");
 		exit(EXIT_FAILURE);
 	}
 	return configFile;
 }
-int main(int argc, char** argv) {
+int validarArchivo(char* ruta)
+{
+	return access(ruta, F_OK );
+}
+void crearArchivo(char* ruta)
+{
+	FILE * file = fopen(ruta,"a");
+	fclose(file);
+
+	//TODO: asignar 1 bloque de datos.
+}
+void borrarArchivo(char* ruta)
+{
+	remove(ruta);
+
+	//TODO: marcar bloques libres dentro del bitmap.
+}
+void leerBloque(int bloque)
+{
+	char* ruta = strcat(rutaBloques, bloque);
+	FILE* file = fopen(ruta, "rb");
+}
+void leerDatosArchivo(t_pedido_datos_fs datosFs)
+{
+	t_metadata_archivo metadataArchivo = leerArchivo(datosFs.ruta);
+	int i = 0;
+	while (metadataArchivo.bloques[i] >= 0 && metadataArchivo.bloques[i] != NULL)
+	{
+		leerBloque(metadataArchivo.bloques[i]);
+		i++;
+	}
+}
+t_metadata_archivo leerArchivo(char* ruta)
+{
+	t_metadata_archivo archivoMetadata;
+	char* k;
+	FILE* archivo = fopen(ruta, "rb");
+
+
+	fclose(archivo);
+	return archivoMetadata;
+}
+
+
+int main(int argc, char** argv)
+{
+	t_metadata_archivo archivo = leerArchivo("/mnt/SADICA_FS/archivos/alumnosSIGA.bin");
 	t_config* configFile = cargarConfiguracion(argv[1]);
 	printf("PUERTO: %d\n",puerto);
 	printf("PUNTO_MONTAJE: %s\n",punto_montaje);
@@ -40,6 +89,10 @@ int main(int argc, char** argv) {
 	dictionary_put(diccionarioHands,"HKEFS","HFSKE");
 	dictionary_put(diccionarioFunc,"ERROR_FUNC",&error);
 	dictionary_put(diccionarioFunc,"KEY_PRINT",&mostrarMensaje);
+	dictionary_put(diccionarioFunc, "VALIDAR_ARCH", &validarArchivo);
+	dictionary_put(diccionarioFunc, "CREAR_ARCH", &crearArchivo);
+	dictionary_put(diccionarioFunc, "BORRAR_ARCH", &borrarArchivo);
+	dictionary_put(diccionarioFunc, "LEER_ARCH", &leerDatosArchivo);
 
 	int sock= crearHostMultiConexion(puerto);
 	correrServidorMultiConexion(sock,NULL,NULL,NULL,diccionarioFunc,diccionarioHands);
--- FileSystem/src/FileSystem.h
+++ FileSystem/src/FileSystem.h
@@ -8,7 +8,34 @@
 #ifndef FILESYSTEM_H_
 #define FILESYSTEM_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <panicommons/panisocket.h>
+#include <panicommons/serializacion.h>
+#include <commons/config.h>
+
+//#define foreach(item, array) \
+//    for(int keep = 1, \
+//            count = 0,\
+//            size = sizeof (array) / sizeof *(array); \
+//        keep && count != size; \
+//        keep = !keep, count++) \
+//      for(item = (array) + count; keep; keep = !keep)
+
+typedef struct
+{
+	int32_t tamanio;
+	char** bloques;
+} t_metadata_archivo;
+
 int puerto;
 char * punto_montaje;
+char* rutaBloques;
+
+void mostrarMensaje(char* mensajes,int socket);
+int validarArchivo(char* ruta);
+void crearArchivo(char* ruta);
+void borrarArchivo(char* ruta);
+void leerDatosArchivo(t_pedido_datos_fs datosFs);
 
 #endif /* FILESYSTEM_H_ */
