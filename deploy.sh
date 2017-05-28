01b76a6e27ad6db5bc96db6c2a2cbd6279f098cf
Martin Picollo <mpicollo.utn@gmail.com>
2017-05-27 23:55:44 -0300

    Arreglos varios comando deploy

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,10 @@
 #! /bin/sh
 
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/workspace/tp-2017-1c-utn-panic/PANICommons/Debug" >> .bashrc
-
 #Bajo e instalo las commons
 echo "Instalando las commons..."
 
 git clone https://github.com/sisoputnfrba/so-commons-library.git
-cd ~/workspace/so-commons-library
+cd /so-commons-library
 sudo make install
 utnso
 echo "Se instalaron las commons"
@@ -15,12 +13,20 @@ echo "Se instalaron las commons"
 echo "Instalando el parser de ansisop..."
 
 git clone https://github.com/sisoputnfrba/ansisop-parser.git
-cd ~/workspace/ansisop-parser
+cd /ansisop-parser
 sudo make install
 utnso
 echo "Se instalo el parser de ansisop"
 
 mv ~/workspace/tp-2017-1c-utn-panic/PANICommons/makefiles ~/workspace/tp-2017-1c-utn-panic/PANICommons/Debug
+
+echo "Se copian los archivos de configuracion"
+cp ~/workspace/tp-2017-1c-utn-panic/Kernel/src/config.txt ~/workspace/tp-2017-1c-utn-panic/Kernel/makefiles
+cp ~/workspace/tp-2017-1c-utn-panic/CPU/src/config.txt ~/workspace/tp-2017-1c-utn-panic/CPU/makefiles
+cp ~/workspace/tp-2017-1c-utn-panic/Memoria/src/config.txt ~/workspace/tp-2017-1c-utn-panic/Memoria/makefiles
+cp ~/workspace/tp-2017-1c-utn-panic/FileSystem/src/config.txt ~/workspace/tp-2017-1c-utn-panic/FileSystem/makefiles
+cp ~/workspace/tp-2017-1c-utn-panic/Consola/src/config.txt ~/workspace/tp-2017-1c-utn-panic/Consola/makefiles 
+
 
 #Compilo los proyectos
 echo "Compilando..."
@@ -36,7 +42,9 @@ cd ~/workspace/tp-2017-1c-utn-panic/FileSystem/makefiles
 make all
 cd ~/workspace/tp-2017-1c-utn-panic/Consola/makefiles
 make all
-echo "Fin compilacion"
 
+export LD_LIBRARY_PATH=:/home/utnso/workspace/tp-2017-1c-utn-panic/PANICommons/Debug:~/workspace/tp-2017-1c-utn-panic/PANICommons/Debug
+
+echo "Fin compilacion"
 
 echo "Deploy finalizado exitosamente"
