d7565ba59aa309af9c0f0dc49daec9de15dbfc97
andresbiso <biso.andres@gmail.com>
2017-07-06 23:18:25 -0300

    [Feat] Agrega varios cambios para reflejar necesidades prueba heap y memoria

---
+++ CPU/makefiles/src/PrimitivasAnsisop.d
@@ -0,0 +1,13 @@
+src/PrimitivasAnsisop.d: ../src/PrimitivasAnsisop.c \
+ ../src/PrimitivasAnsisop.h \
+ /home/utnso/workspace/tp-2017-1c-utn-panic/PANICommons/panicommons/serializacion.h \
+ /home/utnso/workspace/tp-2017-1c-utn-panic/PANICommons/panicommons/panisocket.h \
+ /home/utnso/workspace/tp-2017-1c-utn-panic/PANICommons/panicommons/panicommons.h
+
+../src/PrimitivasAnsisop.h:
+
+/home/utnso/workspace/tp-2017-1c-utn-panic/PANICommons/panicommons/serializacion.h:
+
+/home/utnso/workspace/tp-2017-1c-utn-panic/PANICommons/panicommons/panisocket.h:
+
+/home/utnso/workspace/tp-2017-1c-utn-panic/PANICommons/panicommons/panicommons.h:
